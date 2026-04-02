78a570bb2259734da42c2f867d8d8f6c07d79360
Donovan <donovanjames.2025@gmail.com>
2026-04-02 11:09:25 -0700

    This fixes the blocky data read

--- test1/test1.ino
+++ test1/test1.ino
@@ -2,11 +2,12 @@ const int PDIODE = 34;
 
 void setup() {
   pinMode(PDIODE,INPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 //this doesnt seem to exhibit the 60 Hz-ish interference
 
 void loop() {
   Serial.println(analogRead(PDIODE));
+  delay(10);
 }
