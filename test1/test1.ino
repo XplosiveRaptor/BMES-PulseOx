2b6422db890c32596e0ea8045216c9fb2859bf56
Donovan <donovanjames.2025@gmail.com>
2026-04-02 12:01:02 -0700

    9600 baud

--- test1/test1.ino
+++ test1/test1.ino
@@ -2,12 +2,12 @@ const int PDIODE = 34;
 
 void setup() {
   pinMode(PDIODE,INPUT);
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 //this doesnt seem to exhibit the 60 Hz-ish interference
 
 void loop() {
   Serial.println(analogRead(PDIODE));
-  delay(10);
+  delay(1);
 }
